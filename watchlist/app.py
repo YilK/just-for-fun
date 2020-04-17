06778749b466b8b20fdf6e760b834007a14d1b17
Yilk <huangjk0311@126.com>
2020-04-17 21:33:41 +0800

    Add index page

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+#-*-coding: utf-8 -*-
+from flask import Flask,render_template
 app=Flask(__name__)
+name='YilK'#准备虚拟数据
+movies=[
+    {'title':'My Neighbor Totoro','year':'1988'},
+    {'title':'Dead Poet society','year':'1989'},
+    {'title':'A Perfect	World','year':'1993'},
+    {'title':'Leon','year':'1994'},
+    {'title':'Mahjong','year':'1996'},
+    {'title':'Swallowtail Butterfly','year':'1996'},
+    {'title':'King of Comedy','year':'1999'},
+    {'title':'Devils on	the	Doorstep','year':'1999'},
+    {'title':'WALL-E','year':'2008'},
+    {'title':'The Pork of Music','year':'2012'},
+]
 @app.route('/')
-@app.route('/hjk_is_dsb')
-@app.route('/yilk')
-def hello():
-    return '<h1>Hello	Totoro!</h1><img	src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
+# def hello():
+#     return '<h1>Hello	Totoro!</h1><img	src="http://helloflask.com/totoro.gif">'
 
 #使用了内置的开发服务器来运行程序，这个服务器默认监听本地机的5000端口
 #note 内置的开服务器只能用于开发时使用，部署上线的时候要换性能更好的服务器
